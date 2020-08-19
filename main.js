19627e8a066b9fbce6bb884442472c186405e3d0
Nataliya <n.jelonkina@mail.ru>
2020-08-19 22:01:02 +0300

    second task hw2

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const secondRow = 'собака друг человека';
 //"а" и вывести ту строку, в которой таких букв больше.
 
 
-
 function getRow(firstRow, secondRow) {
 
   let firstCounter = 0;
@@ -33,4 +32,14 @@ function getRow(firstRow, secondRow) {
 }
 
 
-console.log(getRow(firstRow, secondRow)); // мама мыла раму
+//console.log(getRow(firstRow, secondRow)); // мама мыла раму
+
+
+let input = prompt('Введите номер телефона');
+
+function formattedPhone(phone) {
+  return `${phone.charAt(0)}${phone.charAt(1)} (${phone.charAt(2)}${phone.charAt(3)}${phone.charAt(4)}) ${phone.charAt(5)}${phone.charAt(6)}${phone.charAt(7)}-${phone.charAt(8)}${phone.charAt(9)}-${phone.charAt(10)}${phone.charAt(11)}`;
+}
+
+alert( `Твой номер ${formattedPhone(input)}`); 
+//console.log(formattedPhone('+71234567890')); // +7 (123) 456-78-90
