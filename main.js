f05ad0c039f3c18f817f616035cbd21f293209fe
Nataliya <n.jelonkina@mail.ru>
2020-08-21 18:57:23 +0300

    fix by feedback

--- main.js
+++ main.js
@@ -5,34 +5,35 @@ const secondRow = 'собака друг человека';
 //"а" и вывести ту строку, в которой таких букв больше.
 
 
+let firstCounter = 0;
+let secondCounter = 0;
+
+
+function countChar(someRow, letter='а') {
+  let counter = 0;
+
+  for (let i=0; i < someRow.length; i++) {
+    if (someRow.charAt(i) === letter ) {
+      counter++;
+    }
+  }
+  return counter;
+}
+
+
 function getRow(firstRow, secondRow) {
 
-  let firstCounter = 0;
-  let secondCounter = 0;
-
-
-  for (let i=0; i < firstRow.length; i++) {
-    if (firstRow.charAt(i) === 'а') {
-      firstCounter++;
-    }
-  }
-
-  for (let i=0; i < secondRow.length; i++) {
-    if (secondRow.charAt(i) === 'а') {
-      secondCounter++;
-    }
-  }
+  firstCounter = countChar(firstRow);
+  secondCounter = countChar(secondRow);
 
   if (firstCounter > secondCounter) {
     return firstRow;
   } else {
     return secondRow;
   }  
-
 }
 
-
-//console.log(getRow(firstRow, secondRow)); // мама мыла раму
+console.log(getRow(firstRow, secondRow)); // мама мыла раму
 
 
 let input = prompt('Введите номер телефона');
@@ -42,4 +43,4 @@ function formattedPhone(phone) {
 }
 
 alert( `Твой номер ${formattedPhone(input)}`); 
-//console.log(formattedPhone('+71234567890')); // +7 (123) 456-78-90
+console.log(formattedPhone('+71234567890')); // +7 (123) 456-78-90
