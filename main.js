255bbbe24e9de6f9947faf63380e5ec8c85bd692
Nataliya <n.jelonkina@mail.ru>
2020-08-22 08:00:39 +0300

    hw3 base

--- main.js
+++ main.js
@@ -1,46 +1,62 @@
-const firstRow = 'мама мыла раму';
-const secondRow = 'собака друг человека';
+$btn = document.getElementById('btn-kick');
 
-//В каждой из них необходимо посчитать количество букв 
-//"а" и вывести ту строку, в которой таких букв больше.
+const character = {
+  name: 'pickachu',
+  defaultHP: 100,
+  damageHP: 100,
+  elHP: document.getElementById('health-character'),
+  elProgressBar: document.getElementById('progressbar-character')
+}
+
+const enemy = {
+  name: 'Charmander',
+  defaultHP: 100,
+  damageHP: 90,
+  elHP: document.getElementById('health-enemy'),
+  elProgressBar: document.getElementById('progressbar-enemy')
+
+}
 
 
-let firstCounter = 0;
-let secondCounter = 0;
+$btn.addEventListener('click', function() {
+  console.log('btn click');
+  changeHP(character, random(20));
+  changeHP(enemy, random(20));
+});
+
+function init() {
+  console.log('Start game!');
+  renderHP(character);
+  renderHP(enemy);
+}
+
+function renderHP(person) {
+  renderHPLife(person);
+  renderProgressBar(person);  
+} 
 
 
-function countChar(someRow, letter='а') {
-  let counter = 0;
+function renderHPLife(person) {
+  person.elHP.innerText = person.damageHP + ' / ' + person.defaultHP;
+}
 
-  for (let i=0; i < someRow.length; i++) {
-    if (someRow.charAt(i) === letter ) {
-      counter++;
-    }
+function renderProgressBar(person) {
+  person.elProgressBar.style.width = person.damageHP + '%';
+}
+
+init();
+
+function changeHP(person, count){
+  if (person.damageHP < count) {
+    person.damageHP = 0;
+    alert( person.name + ' проиграл!!!');
+    } else {
+    person.damageHP -= count;
   }
-  return counter;
+  
+  renderHP(person);
 }
 
-
-function getRow(firstRow, secondRow) {
-
-  firstCounter = countChar(firstRow);
-  secondCounter = countChar(secondRow);
-
-  if (firstCounter > secondCounter) {
-    return firstRow;
-  } else {
-    return secondRow;
-  }  
+function random(num) {
+  return Math.ceil(Math.random() * num);
 }
-
-console.log(getRow(firstRow, secondRow)); // мама мыла раму
-
-
-let input = prompt('Введите номер телефона');
-
-function formattedPhone(phone) {
-  return `${phone.charAt(0)}${phone.charAt(1)} (${phone.charAt(2)}${phone.charAt(3)}${phone.charAt(4)}) ${phone.charAt(5)}${phone.charAt(6)}${phone.charAt(7)}-${phone.charAt(8)}${phone.charAt(9)}-${phone.charAt(10)}${phone.charAt(11)}`;
-}
-
-alert( `Твой номер ${formattedPhone(input)}`); 
-console.log(formattedPhone('+71234567890')); // +7 (123) 456-78-90
