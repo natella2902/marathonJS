bfe55735b5211d36d62f9fc3e3d9cbb0157aba52
Nataliya <n.jelonkina@mail.ru>
2020-08-24 23:34:57 +0300

    homework-3

--- main.js
+++ main.js
@@ -5,56 +5,64 @@ const character = {
   defaultHP: 100,
   damageHP: 100,
   elHP: document.getElementById('health-character'),
-  elProgressBar: document.getElementById('progressbar-character')
+  elProgressBar: document.getElementById('progressbar-character'),
+  renderHPLife: renderHPLife,
+  renderProgressBar: renderProgressBar,
+  renderHP: renderHP,
+  changeHP: changeHP
 }
 
 const enemy = {
   name: 'Charmander',
   defaultHP: 100,
-  damageHP: 90,
+  damageHP: 100,
   elHP: document.getElementById('health-enemy'),
-  elProgressBar: document.getElementById('progressbar-enemy')
-
+  elProgressBar: document.getElementById('progressbar-enemy'),
+  renderHPLife: renderHPLife,
+  renderProgressBar: renderProgressBar,
+  renderHP: renderHP,
+  changeHP: changeHP
 }
 
 
 $btn.addEventListener('click', function() {
   console.log('btn click');
-  changeHP(character, random(20));
-  changeHP(enemy, random(20));
+  character.changeHP.apply(character, [random(20)]);
+  enemy.changeHP.apply(enemy, [random(20)]);
 });
 
 function init() {
   console.log('Start game!');
-  renderHP(character);
-  renderHP(enemy);
+  character.renderHP();
+  enemy.renderHP();
 }
 
-function renderHP(person) {
-  renderHPLife(person);
-  renderProgressBar(person);  
+function renderHP() {
+  this.renderHPLife();
+  this.renderProgressBar();    
 } 
 
 
-function renderHPLife(person) {
-  person.elHP.innerText = person.damageHP + ' / ' + person.defaultHP;
+
+function renderHPLife() {
+  this.elHP.innerText = this.damageHP + ' / ' + this.defaultHP;
 }
 
-function renderProgressBar(person) {
-  person.elProgressBar.style.width = person.damageHP + '%';
+function renderProgressBar() {
+  this.elProgressBar.style.width = this.damageHP/this.defaultHP*100 + '%';
 }
 
 init();
 
-function changeHP(person, count){
-  if (person.damageHP < count) {
-    person.damageHP = 0;
-    alert( person.name + ' проиграл!!!');
+function changeHP(count){
+  if (this.damageHP < count) {
+    this.damageHP = 0;
+    alert( this.name + ' проиграл!!!');
     } else {
-    person.damageHP -= count;
+    this.damageHP -= count;
   }
   
-  renderHP(person);
+  this.renderHP();
 }
 
 function random(num) {
